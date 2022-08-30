d0c0937cf2cd6176ab097fe74729c8d7e6f99acd
Jamie <31694@ma-web.nl>
2022-08-31 00:11:27 +0200

    a

---
+++ map.js
@@ -0,0 +1,100 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("0").style.opacity = 1;
+    document.getElementById("1").style.opacity = 0;
+    document.getElementById("2").style.opacity = 0;
+    document.getElementById("3").style.opacity = 0;
+    document.getElementById("4").style.opacity = 0;
+    document.getElementById("5").style.opacity = 0;
+    document.getElementById("6").style.opacity = 0;
+
+    document.getElementById("7").style.opacity = 1;
+    document.getElementById("8").style.opacity = 0;
+    document.getElementById("9").style.opacity = 0;
+    document.getElementById("10").style.opacity = 0;
+    document.getElementById("11").style.opacity = 0;
+    document.getElementById("12").style.opacity = 0;
+    document.getElementById("13").style.opacity = 0;
+
+    document.getElementById("17").style.opacity = 1;
+    document.getElementById("18").style.opacity = 0;
+    document.getElementById("19").style.opacity = 0;
+    document.getElementById("20").style.opacity = 0;
+    document.getElementById("21").style.opacity = 0;
+    document.getElementById("22").style.opacity = 0;
+    document.getElementById("23").style.opacity = 0;
+
+    document.getElementById("24").style.opacity = 1;
+    document.getElementById("25").style.opacity = 0;
+    document.getElementById("26").style.opacity = 0;
+    document.getElementById("27").style.opacity = 0;
+    document.getElementById("28").style.opacity = 0;
+    document.getElementById("29").style.opacity = 0;
+    document.getElementById("30").style.opacity = 0;
+    document.getElementById("31").style.opacity = 0;
+    
+    document.getElementById("32").style.opacity = 1;
+    document.getElementById("33").style.opacity = 0;
+    document.getElementById("34").style.opacity = 0;
+    document.getElementById("35").style.opacity = 0;
+    document.getElementById("36").style.opacity = 0;
+    document.getElementById("37").style.opacity = 0;
+    document.getElementById("38").style.opacity = 0;
+    document.getElementById("39").style.opacity = 0;
+});
+
+function showPicture(index) {
+    document.getElementById("0").style.opacity = 0;
+    document.getElementById("1").style.opacity = 0;
+    document.getElementById("2").style.opacity = 0;
+    document.getElementById("3").style.opacity = 0;
+    document.getElementById("4").style.opacity = 0;
+    document.getElementById("5").style.opacity = 0;
+    document.getElementById("6").style.opacity = 0;
+    document.getElementById(index).style.opacity = 1;
+}
+
+function show(index) {
+    document.getElementById("7").style.opacity = 0;
+    document.getElementById("8").style.opacity = 0;
+    document.getElementById("9").style.opacity = 0;
+    document.getElementById("10").style.opacity = 0;
+    document.getElementById("11").style.opacity = 0;
+    document.getElementById("12").style.opacity = 0;
+    document.getElementById("13").style.opacity = 0;
+    document.getElementById(index).style.opacity = 1;
+}
+
+function show2(index) {
+    document.getElementById("17").style.opacity = 0;
+    document.getElementById("18").style.opacity = 0;
+    document.getElementById("19").style.opacity = 0;
+    document.getElementById("20").style.opacity = 0;
+    document.getElementById("21").style.opacity = 0;
+    document.getElementById("22").style.opacity = 0;
+    document.getElementById("23").style.opacity = 0;
+    document.getElementById(index).style.opacity = 1;
+}
+
+function show3(index) {
+    document.getElementById("24").style.opacity = 0;
+    document.getElementById("25").style.opacity = 0;
+    document.getElementById("26").style.opacity = 0;
+    document.getElementById("27").style.opacity = 0;
+    document.getElementById("28").style.opacity = 0;
+    document.getElementById("29").style.opacity = 0;
+    document.getElementById("30").style.opacity = 0;
+    document.getElementById("31").style.opacity = 0;
+    document.getElementById(index).style.opacity = 1;
+}
+
+function show4(index) {
+    document.getElementById("32").style.opacity = 0;
+    document.getElementById("33").style.opacity = 0;
+    document.getElementById("34").style.opacity = 0;
+    document.getElementById("35").style.opacity = 0;
+    document.getElementById("36").style.opacity = 0;
+    document.getElementById("37").style.opacity = 0;
+    document.getElementById("38").style.opacity = 0;
+    document.getElementById("39").style.opacity = 0;
+    document.getElementById(index).style.opacity = 1;
+}
